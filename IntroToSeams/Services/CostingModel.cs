928423e811cfe674ef3c34fa7e981f34df1a94f6
Mitchell Busby <mitchbusby@gmail.com>
2018-04-22 08:56:02 +1000

    Initial commit

---
+++ IntroToSeams/Services/CostingModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace IntroToSeams.Services
+{
+    public class CostingModel
+    {
+        public CostingModel()
+        {
+            
+        }
+
+        public void MakeChargeIfApplicable(int clientId, int numberOfSenders) {
+            // Does some voodoo
+            // TODO: generate voodoo
+
+
+            ExampleRepository.Instance.ChargeClient(numberOfSenders);
+        }
+
+        public bool ChargeClient(int numberOfSenders) {
+            return ExampleRepository.Instance.ChargeClient(numberOfSenders);
+        }
+    }
+
+    public class TestCostingModel : CostingModel {
+        public override bool ChargeClient(int numberOfSenders) {
+            
+        }
+    }
+}
---
+++ IntroToSeams/Services/ExampleLegacyRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace IntroToSeams.Services
+{
+    public class ExampleRepository
+    {
+        private static ExampleRepository instance = null;
+
+        public ExampleRepository()
+        {
+        }
+
+        public static ExampleRepository Instance {
+            get {
+                if (instance == null) {
+                    instance = new ExampleRepository();
+                }
+
+                return instance;
+            }
+        }
+
+        public bool ChargeClient(int numberOfSenders)
+        {
+            Thread.Sleep(5000);
+            return true;
+        }
+    }
+}
